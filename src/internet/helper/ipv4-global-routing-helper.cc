f3c32b923de8bfe7e644c2d4a5d79ef05b459786
Alexander Afanasyev <alexander.afanasyev@ucla.edu>
2012-01-20 17:32:39 -0800

    internet: Solving very serious bug in Ipv4GlobalRoutingHelper
    Somehow, the metric values for "disabled faces" in
    PopulateAllPossibleRoutingTables function were set to random values,
    instead of UINT16_MAX.  This caused a lot of unexpected thing that were
    hard to track down.

--- src/internet/helper/ipv4-global-routing-helper.cc
+++ src/internet/helper/ipv4-global-routing-helper.cc
@@ -94,7 +94,6 @@ Ipv4GlobalRoutingHelper::PopulateAllPossibleRoutingTables (void)
           originalMetric[iface] = ipv4->GetMetric (iface);
         }
 
-      UniformVariable m_rand;
       // enable interfaces one by one and calculate routes
       for (uint32_t enabledInterface = 1; enabledInterface < ipv4->GetNInterfaces (); enabledInterface++)
         {
@@ -102,7 +101,7 @@ Ipv4GlobalRoutingHelper::PopulateAllPossibleRoutingTables (void)
           
           for (uint32_t iface = 1; iface < ipv4->GetNInterfaces (); iface++)
             {
-              ipv4->SetMetric (iface,  m_rand.GetInteger (1, UINT16_MAX));
+              ipv4->SetMetric (iface, UINT16_MAX);
             }
           ipv4->SetMetric (enabledInterface, originalMetric[enabledInterface]);
 
